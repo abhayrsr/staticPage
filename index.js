7de5141d6ca277c9c3512f020e35f0b81cc7637e
abhay <abhayrajsinghrana123@gmail.com>
2023-07-03 21:23:28 +0530

    feat: component

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // ReactDOM.render(<h1> Hello Everyone!!</h1>, document.getElementById("root"));
-ReactDOM.render(<ul><li>Banana</li><li>Orange</li><li>Apple</li></ul>, document.getElementById("list"));
+// ReactDOM.render(<ul><li>Banana</li><li>Orange</li><li>Apple</li></ul>, document.getElementById("list"));
 
-function mainContent(){
-    return <h1>I'm learning react</h1>
+function MainContent(){
+    return (
+    <h1>I'm learning react</h1>
+    )
 }
 
 ReactDOM.render(
     <div>
-        <mainContent />
+        <MainContent />
     </div>,
     document.getElementById("root")
 )
