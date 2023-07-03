18ed6242d13b3a1b28ebdf51ddb70805646db214
abhay <abhayrajsinghrana123@gmail.com>
2023-07-03 22:16:00 +0530

    feat: list

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const page = (
 
 ReactDOM.render(page, document.getElementById("list"));
 
+const list =(
+    <nav>
+        <h1 className = "website"> My Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Career</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(list, document.getElementById("nav"))
+
 function MainContent(){
     return (
     <h1>I'm learning react</h1>
