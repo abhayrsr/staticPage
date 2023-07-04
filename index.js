845eb4de58ab25f2b41bdf699b07d562ec2b4191
abhay <abhayrajsinghrana123@gmail.com>
2023-07-04 18:22:59 +0530

    feat: image

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
+function Image(){
+    return (
+        <header>
+            <nav className = "nav">
+               <img src = "React-icon.svg (1).png" width = "70px"/>
+               <ul className = "nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+               </ul>
+            </nav>
+            
+        </header>
+    )
+}
 
-const head = (
-   <div><h1> Fun Facts About React</h1></div>
-)
+function Header(){
+    return (
+        <h1>Reasons I'm excited about React</h1>
+    )
+}
 
-const list = (
-    <div>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars over GitHub</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
+function List(){
+    return (
+        <ol>
+            <li>It's a popular library, so l'll be able to fit in with cool kids</li>
+            <li>I'm more likely to get a job as a developer if I know React</li>
+        </ol>
+    )
+}
 
-ReactDOM.render(head, document.getElementById("head"))
-ReactDOM.render(list, document.getElementById("list"))
+function Footer(){
+    return (
+        <footer>
+            <smalll>© 2021 Ziroll development. All rights reserved</smalll>
+        </footer>
+    )
+}
+
+ReactDOM.render(<Image />, document.getElementById("img"))
+ReactDOM.render(<Header />, document.getElementById("head"))
+ReactDOM.render(<List />, document.getElementById("list"))
+ReactDOM.render(<Footer />, document.getElementById("footer"))
