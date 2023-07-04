a44e2579e1e40a5b37ca59367109679bafc37d42
abhay <abhayrajsinghrana123@gmail.com>
2023-07-04 13:36:55 +0530

    feat: reactRender

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom"
 
-const heading = (
-    <div><h1>Fun Facts About React</h1></div>
+const head = (
+   <div><h1> Fun Facts About React</h1></div>
 )
 
-// const list = (
-//     <div>
-//         <ul>
-//             <li>Was first released in 2013</li>
-//             <li>Was originally created by Jordan Walke</li>
-//             <li>Has well over 100k stars over GitHub</li>
-//             <li>Powers thousands of enterprise apps, including mobile apps</li>
-//         </ul>
-//     </div>
-// )
+const list = (
+    <div>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars over GitHub</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
 
-ReactDOM.render(heading, document.getElementById("root"))
+ReactDOM.render(head, document.getElementById("head"))
 ReactDOM.render(list, document.getElementById("list"))
